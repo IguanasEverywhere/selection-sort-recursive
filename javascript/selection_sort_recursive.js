17ec77870dec3c8aa4c143ff6901978a9bf3cd20
Scott Schwab <scottschwab86@gmail.com>
2024-02-04 15:41:16 -0600

    Sort of complete challenge WILL REVIEW

--- javascript/selection_sort_recursive.js
+++ javascript/selection_sort_recursive.js
@@ -1,5 +1,22 @@
 function selectionSortRecursive(arr) {
   // type your code here
+  // logic is to get rid of smallest each time and build up array from there
+
+  if (arr.length === 0) {
+    return []
+  }
+
+  let minVal = Math.min(...arr)
+  let minIdx = arr.indexOf(minVal)
+
+  arr.splice(minIdx, 1)
+  // remember it returns the value of the recursive call to HERE, where it was called. min and minIdx are not re-declared, it's already there in the stack
+  let result = selectionSortRecursive(arr)
+  result.unshift(minVal)
+  return result
+
+
+
 }
 
 if (require.main === module) {
